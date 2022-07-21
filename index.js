397ec5cd598e41aa34c5674d59a318e6dc7b1596
ngonpaul <pmgonzalez05@outlook.com>
2022-07-21 09:30:45 -0700

    Added .gitignore and content

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const server = express(); // This server is deaf
+
+server.listen(3000); // Told the server to listen on port 3000
+
+const destinationsDB = {
+  123456: {
+    destination: "Eiffel Tower",
+    location: "Paris",
+    photo:
+      "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  234567: {
+    destination: "Big Ben",
+    location: "London",
+    photo:
+      "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+  },
+};
+
+// CREATE (OPTIONAL)
+
+// READ => DO THIS
+// GET /destinations => send back the whole db
+server.get("/destinations", (req, res) => {
+  res.send(destinationsDB);
+});
+
+// UPDATE (OPTIONAL)
+
+// DELETE (OPTIONAL)
